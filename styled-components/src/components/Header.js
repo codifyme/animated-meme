f951847a9d205f7b4c883bf8f12c0e714990858d
Angel Cochachez <angelcochachez@Angels-MacBook-Pro.local>
2022-03-29 15:48:17 -0700

    Added Header and container styled

--- styled-components/src/components/Header.js
+++ styled-components/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { StyledHeader } from './styles/Header.styled'
 
 export default function Header() {
   return (
-    <header>
+    <StyledHeader>
         <h1>Hubble</h1>
-    </header>
+    </StyledHeader>
   )
 }
