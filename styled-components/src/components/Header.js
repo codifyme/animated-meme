b8f0e5a7107dccd34bc955b19e5b49f8b42f0852
Angel Cochachez <angelcochachez@Angels-MacBook-Pro.local>
2022-03-30 13:22:36 -0700

    initialize project using creating button styled, header and container styled

--- styled-components/src/components/Header.js
+++ styled-components/src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { StyledHeader } from './styles/Header.styled'
+import { Container } from './styles/Container.styled'
 
 export default function Header() {
   return (
     <StyledHeader>
-        <h1>Hubble</h1>
+        <Container>
+            <h1>Hubble</h1>
+        </Container>
+        
     </StyledHeader>
   )
 }
